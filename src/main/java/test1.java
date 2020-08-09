130df433d97b22bcdd1e4cc74cbe2d779d2c7322
koolkarniatul <koolkarniatul@gmail.com>
2020-08-09 15:37:58 -0500

    test

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -5,8 +5,11 @@ public class test1 {
     public static void main(String [] args)
 
     {
+
+
         System.out.println("Hello World");
 
+        System.out.println("Hello World Success");
     }
 
 }
